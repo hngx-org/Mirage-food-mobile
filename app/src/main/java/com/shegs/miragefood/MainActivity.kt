6921b92397ae8ff7a904ed40b5838eca998c3bac
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-09-19 00:25:09 +0100

    Initial commit

---
+++ app/src/main/java/com/shegs/miragefood/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.shegs.miragefood
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shegs.miragefood.ui.theme.MirageFoodTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MirageFoodTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    StartScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun StartScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = "Hello World")
+    }
+
+}
