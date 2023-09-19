3265d9c4fd3f0461c8f817ffe1e599a40d892c1a
Chinaza Blossom <chinazablossom7@gmail.com>
2023-09-19 20:38:51 +0100

    created a card with image and text

--- app/src/main/java/com/shegs/miragefood/MainActivity.kt
+++ app/src/main/java/com/shegs/miragefood/MainActivity.kt
@@ -3,34 +3,13 @@ package com.shegs.miragefood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.shegs.miragefood.ui.theme.MirageFoodTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,62 +27,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Composable
-fun CardWithImageAndText(imageResId: Int, greeting: String, description: String) {
-    Card(
-        modifier = Modifier
-            .background(Color.White)
-            .fillMaxWidth()
-            .padding(16.dp)
-            .background(Color.White),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(Color.White)
 
-    ) {
-        Box {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-            ) {
-                Text(
-                    text = greeting,
-                    textAlign = TextAlign.Start,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-
-                Text(
-                    text = description,
-                    fontSize = 14.sp,
-                    textAlign = TextAlign.Start
-                )
-            }
-
-            Image(
-                painter = painterResource(id = imageResId),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(36.dp)
-                    .align(Alignment.TopEnd)
-                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                    .background(Color.White),
-                contentScale = ContentScale.Crop,
-            )
-        }
-    }
-}
-
-
-@Preview
-@Composable
-fun CardPreview() {
-    CardWithImageAndText(
-        imageResId = R.drawable.ic_launcher_background, greeting = "Hey Amy 👋",
-        description = "Send them that free lunch today !"
-    )
-}
 @Composable
 fun StartScreen() {
 
