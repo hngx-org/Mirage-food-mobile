d18e8d501aeef8e865f1cd8c4e0ba9258a304ba3
ani <godwin@istrategytech.com>
2023-09-19 19:59:15 +0100

    wip: topnavigationbar

--- app/src/main/java/com/shegs/miragefood/utils/TopNav.kt
+++ app/src/main/java/com/shegs/miragefood/utils/TopNav.kt
@@ -1,27 +1,58 @@
 package com.shegs.miragefood.utils
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.shegs.miragefood.ui.theme.Typography
+import com.shegs.miragefood.ui.theme.grey
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopNavigationBar(text: String, onBackButtonPressed: () -> Unit) {
+fun TopNavigationBar(
+    title: String,
+    subtitle: String? = "",
+    useCancelButton: Boolean? = false,
+    onBackButtonPressed: () -> Unit
+) {
     TopAppBar(
         title = {
-            Text(text, style = Typography.titleLarge.copy(fontWeight = FontWeight.W500))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 14.dp)
+            ) {
+                Text(title, style = Typography.titleLarge.copy(fontWeight = FontWeight.W500))
+                Text(
+                    subtitle!!,
+                    style = Typography.bodySmall.copy(
+                        fontSize = 12.sp,
+                        color = grey,
+                        fontWeight = FontWeight.W400
+                    )
+                )
+            }
         },
         navigationIcon = {
             IconButton(onClick = onBackButtonPressed) {
-                Icon(Icons.Filled.ArrowBack, "arrow back")
+                if (!useCancelButton!!) Icon(Icons.Filled.ArrowBack, "arrow back")
+                else Icon(Icons.Filled.Close, "arrow back")
             }
         },
     )
@@ -30,7 +61,7 @@ fun TopNavigationBar(text: String, onBackButtonPressed: () -> Unit) {
 @Preview
 @Composable
 fun Preview() {
-    TopNavigationBar(text = "20,000") {
+    TopNavigationBar(title = "20,000", subtitle = "Your Free Lunch Redeemed Balance") {
 
     }
 }
