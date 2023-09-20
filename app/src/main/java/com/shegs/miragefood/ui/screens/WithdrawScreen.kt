15a987a9b4262a4200622f64d5a22b8b077fcf7d
Great Grant <greatgrant116@gmail.com>
2023-09-20 16:07:38 +0100

    create modal bottom sheet for withdrawal

--- app/src/main/java/com/shegs/miragefood/ui/screens/WithdrawScreen.kt
+++ app/src/main/java/com/shegs/miragefood/ui/screens/WithdrawScreen.kt
@@ -1,5 +1,6 @@
 package com.shegs.miragefood.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,36 +10,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.shegs.miragefood.R
-import com.shegs.miragefood.utils.TopNavigationBar
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.shegs.miragefood.utils.RoundedCornerButton
+import com.shegs.miragefood.utils.TopNavigationBar
 
 @Composable
 fun WithdrawScreen() {
+    var isSheetOpen by rememberSaveable{ mutableStateOf(false) }
+
+    TopNavigationBar(text = stringResource(id = R.string.withdraw_screen_label,)) {
+        
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Box(
@@ -86,22 +91,24 @@ fun WithdrawScreen() {
         }
 
         Row(
-            modifier = Modifier.fillMaxWidth()
-                .padding(bottom =20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 5.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             ElevatedButton(
                 onClick = { /* Handle button 1 click */ },
-                modifier = Modifier.weight(1f).background(color = Color(0x1A967BB6))
+                modifier = Modifier
+                    .weight(1f)
             ) {
                 Text(text = "₦20,000")
             }
             Spacer(modifier = Modifier.width(16.dp))
             ElevatedButton(
             onClick = { /* Handle button 1 click */ },
-                modifier = Modifier.weight(1f).background(color = Color(0x1A967BB6),
-                    shape = RoundedCornerShape(size = 8.dp)
-                )
+                modifier = Modifier
+                    .weight(1f)
+
 
             ) {
                 Text(
@@ -120,7 +127,7 @@ fun WithdrawScreen() {
 
             )
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(5.dp))
         Card(
             modifier = Modifier
                 .fillMaxWidth()
@@ -130,7 +137,8 @@ fun WithdrawScreen() {
             )
         ) {
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(16.dp)
                     .fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -160,7 +168,8 @@ fun WithdrawScreen() {
             )
         ) {
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(16.dp)
                     .fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -182,13 +191,106 @@ fun WithdrawScreen() {
         }
 
         Button(
-            onClick = { /* Handle button click */ },
+            onClick = {
+                //Todo() handle logic to open bottom sheet
+                isSheetOpen = true
+                      },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
             Text(text = "Withdraw")
         }
+    }
+    FullScreenBottomSheet(
+        isSheetOpen, onDismissSheet = {
+        isSheetOpen = false
+    })
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun FullScreenBottomSheet(isSheetOpen: Boolean, onDismissSheet: () -> Unit) {
+    var amount by remember { mutableStateOf("2000") }
+    val sheetState = rememberModalBottomSheetState()
+
+
+
+    if(isSheetOpen){
+        ModalBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { onDismissSheet}
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.group_right),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(8.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.celebrate),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(shape = CircleShape)
+                        .background(Color.Gray)
+                        .align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Congratulations!",
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "₦$amount has been added to your\n" +
+                            "wallet",
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Have Fun",
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.group_left),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(shape = CircleShape)
+                        .background(Color.Gray)
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+fun CustomText(text: String) {
+    Box(
+        modifier = Modifier
+            .size(150.dp)
+            .background(
+                color = Color(0x1A967BB6), // Background color
+                shape = RoundedCornerShape(8.dp) // Corner radius
+            )
+            .padding(16.dp),
+        contentAlignment = androidx.compose.ui.Alignment.Center
+    ) {
+        Text(
+            text = "Custom Text",
+            color = Color.White, // Text color
+        )
     }
 }
 
