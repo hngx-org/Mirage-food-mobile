07bc73640486a9070de0eb406329c0f74e863d14
ani <godwin@istrategytech.com>
2023-09-20 14:33:07 +0100

    wip: bottomsheet

--- app/src/main/java/com/shegs/miragefood/utils/BottomSheet.kt
+++ app/src/main/java/com/shegs/miragefood/utils/BottomSheet.kt
@@ -1,15 +1,24 @@
 package com.shegs.miragefood.utils
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shegs.miragefood.ui.theme.Typography
@@ -24,21 +33,32 @@ fun BottomSheet(
     title: String,
     description: String,
     secondDescription: String,
+    onButtonClicked: () -> Unit,
 ) {
     ModalBottomSheet(sheetState = sheetSate, onDismissRequest = { /*TODO*/ }) {
-        Text(text = title, style = Typography.bodyMedium.copy(fontWeight = FontWeight.W500))
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            text = description,
-            style = Typography.bodySmall.copy(fontWeight = FontWeight.W400, color = grey3)
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(
-            text = secondDescription,
-            style = Typography.bodyMedium.copy(fontWeight = FontWeight.W400, color = seed)
-        )
-        Spacer(modifier = Modifier.height(40.dp))
-        RoundedCornerButton(text = "Close", onClick = {})
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(text = title, style = Typography.bodyMedium.copy(fontWeight = FontWeight.W500))
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                textAlign = TextAlign.Center,
+                text = description,
+                style = Typography.bodySmall.copy(fontWeight = FontWeight.W400, color = grey3)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = secondDescription,
+                style = Typography.bodyMedium.copy(fontWeight = FontWeight.W400, color = seed)
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            RoundedCornerButton(text = "Close", onClick = onButtonClicked)
+            Spacer(modifier = Modifier.height(46.dp))
+        }
 
     }
 }
@@ -48,7 +68,19 @@ fun BottomSheet(
 @Composable
 fun PreviewBottomSheet() {
     val sheetState = rememberModalBottomSheetState()
-    RoundedCornerButton(text = "Close", onClick = {})
+    var showBottomSheet by remember { mutableStateOf(false) }
+    if (showBottomSheet) {
+        BottomSheet(
+            sheetSate = sheetState,
+            title = "Nicely done!",
+            description = "You’ve just brightened Ken Adam’s day\nwith a free lunch ",
+            secondDescription = "You're a good sport!",
+            onButtonClicked = {}
+        )
+    }
+    RoundedCornerButton(text = "Close", onClick = {
+        showBottomSheet = true
+    })
 
 }
 
