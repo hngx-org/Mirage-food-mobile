ac03d6b8af212372ea661eb65f4208d4344dc1bc
ani <godwin@istrategytech.com>
2023-09-21 13:21:15 +0100

    wip: onboarding screen

--- app/src/main/java/com/shegs/miragefood/models/repositories/OnboardingRepository.kt
+++ app/src/main/java/com/shegs/miragefood/models/repositories/OnboardingRepository.kt
@@ -1,11 +1,22 @@
-//package com.shegs.miragefood.models.repositories
-//
-//import com.shegs.miragefood.models.datas.Transaction
-//import kotlinx.coroutines.flow.MutableStateFlow
-//import kotlinx.coroutines.flow.StateFlow
-//import javax.inject.Inject
-//
-//class OnboardingRepository @Inject constructor() {
-//    private val _onboardingItems = MutableStateFlow<List<Transaction>>(emptyList())
-//    val transactions: StateFlow<List<Transaction>> = _transactions
-//}
+package com.shegs.miragefood.models.repositories
+
+import com.shegs.miragefood.models.datas.OnboardingItems
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class OnboardingRepository @Inject constructor() {
+    private val _onboardingItems = MutableStateFlow<List<OnboardingItems>>(emptyList())
+    val onboardingItems: StateFlow<List<OnboardingItems>> = _onboardingItems
+
+    init {
+        // Fetch or load data from an actual data source here
+        // For simplicity, you can use your sample data here
+        _onboardingItems.value = listOf(
+            OnboardingItems.First,
+            OnboardingItems.Second,
+            OnboardingItems.Third
+        )
+    }
+
+}
--- app/src/main/java/com/shegs/miragefood/viewmodels/OnboardingViewModel.kt
+++ app/src/main/java/com/shegs/miragefood/viewmodels/OnboardingViewModel.kt
@@ -2,29 +2,26 @@ package com.shegs.miragefood.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.shegs.miragefood.models.datas.OnboardingItems
+import com.shegs.miragefood.models.repositories.OnboardingRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
-class OnboardingViewModel @Inject constructor() : ViewModel() {
-    val onboardingPages =
-        listOf(
-            OnboardingItems.First,
-            OnboardingItems.Second,
-            OnboardingItems.Third
-        )
+class OnboardingViewModel @Inject constructor(
+    private val onboardingRepository: OnboardingRepository
+) : ViewModel() {
+    val onboardingPages: StateFlow<List<OnboardingItems>> = onboardingRepository.onboardingItems
 
     private var _pageIndex = MutableStateFlow(0)
-    val pageIndex : StateFlow<Int> = _pageIndex
+    val pageIndex: StateFlow<Int> = _pageIndex
 
-    fun setIndex(index:Int){
+    fun setIndex(index: Int) {
         _pageIndex.value = index
     }
 
 
-
     fun retrieveOnboardingPages(): List<OnboardingItems> {
-        return onboardingPages
+        return onboardingPages.value
     }
 
 
