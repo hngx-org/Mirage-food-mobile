192945b6e51d4f307979e5b16891e93c876c33a7
Great Grant <greatgrant116@gmail.com>
2023-09-21 19:36:14 +0100

    fix withdraw screen layout

--- app/src/main/java/com/shegs/miragefood/ui/screens/WithdrawScreen.kt
+++ app/src/main/java/com/shegs/miragefood/ui/screens/WithdrawScreen.kt
@@ -1,276 +1,258 @@
 package com.shegs.miragefood.ui.screens
 
-import androidx.compose.foundation.Image
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.shegs.miragefood.R
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.shegs.miragefood.utils.EmojiByUnicode
+import com.shegs.miragefood.models.datas.WithdrawnTransaction
+import com.shegs.miragefood.utils.BottomSheet
 import com.shegs.miragefood.utils.TopNavigationBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import com.shegs.miragefood.ui.theme.Typography
+import com.shegs.miragefood.ui.theme.seed
+import com.shegs.miragefood.utils.RoundedCornerButton
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
-fun WithdrawScreen() {
-    var isSheetOpen by rememberSaveable{ mutableStateOf(false) }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .height(141.dp)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "Withdraw redeemed cash to your\n" +
-                            "bank account",
-                    fontSize = 16.sp,
-                    textAlign = TextAlign.Center
-                    )
-
-                Spacer(modifier = Modifier.weight(1f))
-                Text(
-                    text = "₦20,000",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight(500),
-                    )
-                Text(
-                    text = "Redeemed Balance",
-                    fontSize = 14.sp,
-                )
-            }
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Amount",
-                fontSize = 14.sp
-            )
-            Text(
-                text = "Reason (optional)",
-                fontSize = 14.sp
-            )
-        }
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 5.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            ElevatedButton(
-                onClick = { /* Handle button 1 click */ },
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(text = "₦20,000")
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            ElevatedButton(
-            onClick = { /* Handle button 1 click */ },
-                modifier = Modifier
-                    .weight(1f)
-
-
-            ) {
-                Text(
-                    text = "Other",
-                    color = Color(0x331E1E1E),
-                )
-            }
-        }
-
-        Text(
-            text = "To",
-            fontSize = 12.sp,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight(400),
-
-            )
-
-        Spacer(modifier = Modifier.height(5.dp))
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(91.dp),
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 4.dp
-            )
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.account_balance_fill),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "Elizabeth Faith Yaroson")
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "10110000 - KUDA MFB")
-            }
-        }
-
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(91.dp),
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 4.dp
-            )
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.account_balance_fill),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "Elizabeth Faith Yaroson")
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "22058383 - Access Bank")
-            }
-        }
-
-        Button(
-            onClick = {
-                //Todo() handle logic to open bottom sheet
-                isSheetOpen = true
-                      },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(text = "Withdraw")
-        }
-    }
-    FullScreenBottomSheet(
-        isSheetOpen,
-        onDismissSheet = {
-        isSheetOpen = false
-    })
-}
-
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
-@Composable
-fun FullScreenBottomSheet(isSheetOpen: Boolean, onDismissSheet: () -> Unit) {
-    var amount by remember { mutableStateOf("2000") }
+fun WithdrawScreen(
+    withdrawnTransaction: WithdrawnTransaction,
+    closeModal: () -> Unit,
+) {
+    var isSheetOpen by remember { mutableStateOf(false) }
     val sheetState = rememberModalBottomSheetState()
 
-
-
-    if(isSheetOpen){
-        ModalBottomSheet(
-            sheetState = sheetState,
-            onDismissRequest = { onDismissSheet}
-        ){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopNavigationBar(
+                onBackButtonPressed = { TODO()/* Handle back button press */ }
+            )
+        }
+    ) {
+        BottomSheetScaffold(
+            sheetContent = {
+                if (isSheetOpen) {
+                    BottomSheet(
+                        sheetState = sheetState,
+                        title = "Cheers!",
+                        emojiUnicode = 0x1F911,
+                        description = "Your funds have been successfully withdrawn",
+                        secondDescription = "You are all set!"
+                    ) {
+                        // Close the bottom sheet when the "Close" button in the sheet is clicked
+                        isSheetOpen = false
+                    }
+                }
+            },
+            sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+            sheetBackgroundColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            sheetElevation = 16.dp,
+            scaffoldState = rememberBottomSheetScaffoldState(),
+            ) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.group_right),
-                    contentDescription = null,
+                Box(
                     modifier = Modifier
-                        .align(Alignment.End)
-                        .padding(8.dp)
+                        .padding(top = 72.dp)
+                        .height(95.dp)
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Withdraw redeemed cash to your bank account",
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF1E1E1E),
+                        )
+
+                        Spacer(modifier = Modifier.weight(1f))
+                        Text(
+                            text = "20 free lunch",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight(500),
+                        )
+                        Text(
+                            text = "Your free lunch redeemed balance",
+                            fontSize = 14.sp,
+                        )
+                    }
+                }
+
+                TextRowWithTwoTexts(
+                    text1 = "Free lunch amount",
+                    text2 = "Redeemed Cash",
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth()
                 )
-                Spacer(modifier = Modifier.height(16.dp))
-                EmojiByUnicode(emojiUnicode = 0x1F973)
-                Spacer(modifier = Modifier.height(16.dp))
+
+                ButtonRow(onClick1 = { /*TODO*/ }, text1 ="10", onClick2 = { /*TODO*/ }, text2 = "₦20000")
+
+                Spacer(modifier = Modifier.height(5.dp))
+
                 Text(
-                    text = "Congratulations!",
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    style = TextStyle(fontWeight = FontWeight.Bold)
+                    text = "To",
+                    fontSize = 12.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight(400),
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+
+                TextRowWithTwoTexts(
+                    text1 = "Bank Name",
+                    text2 = "Account Name",
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                ButtonRow(onClick1 = { /*TODO*/ }, text1 ="Kuda MFB", onClick2 = { /*TODO*/ }, text2 = "1120000")
+
+                Spacer(modifier = Modifier.height(5.dp))
+
+                TextRowWithTwoTexts(
+                    text1 = "Account Name",
+                    text2 = "Reason",
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                ButtonRow(onClick1 = { /*TODO*/ }, text1 ="Peter Joe", onClick2 = { /*TODO*/ }, text2 = "other")
+                RoundedCornerButton(text = "Withdraw") {
+                    //Todo Handle logic to open sheet
+                    isSheetOpen = true
+                }
+            }
+
+
+
+        }
+
+        }
+    }
+
+@Composable
+fun ButtonRow(
+    onClick1: () -> Unit,
+    text1: String,
+    onClick2: () -> Unit,
+    text2: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Card(
+            modifier = Modifier
+                .background(color = Color(0x1A967BB6), shape = RoundedCornerShape(size = 8.dp))
+                .weight(1f)
+                .clickable { onClick1() },
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            ),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
                 Text(
-                    text = "₦$amount has been added to your\n" +
-                            "wallet",
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                    text = text1,
+                    style = MaterialTheme.typography.bodyMedium.copy(color = seed),
+                    fontWeight = FontWeight(500),
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Card(
+            modifier = Modifier
+                .background(color = Color(0x1A967BB6), shape = RoundedCornerShape(size = 8.dp))
+                .weight(1f)
+                .clickable { onClick2() },
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            ),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
                 Text(
-                    text = "Have Fun",
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.group_left),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(50.dp)
-                        .clip(shape = CircleShape)
-                        .background(Color.Gray)
+                    text = text2,
+                    style = MaterialTheme.typography.bodyMedium.copy(color = seed),
+                    fontWeight = FontWeight(500),
                 )
             }
         }
     }
-
 }
 
-
-@Preview(
-    showBackground = true,
-    showSystemUi = true)
 @Composable
-fun MyUIPreview() {
-        WithdrawScreen()
+fun TextRowWithTwoTexts(
+    text1: String,
+    text2: String,
+    fontSize: TextUnit = 14.sp,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = text1,
+            fontSize = fontSize,
+        )
+        Text(
+            text = text2,
+            fontSize = fontSize,
+        )
+    }
 }
